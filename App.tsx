4c4426fc337bade273ae7a456e3f4401203345eb
NguyenCuuNguyen <nguyen2nk@gmail.com>
2023-11-01 10:17:54 -0400

    working code

--- App.tsx
+++ App.tsx
@@ -1,36 +1,20 @@
-import { Canvas, Path, Skia } from "@shopify/react-native-skia";
-import React from "react";
-import { AppRegistry, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { SketchCanvasWithInteraction } from "./src/canvas-with-interactivity";
+import { SketchCanvasWithoutInteraction } from "./src/canvas-without-interactivity";
+import { SketchCanvasWithInteractionAndCustomization } from "./src/canvas-with-interactivity-and-customizability";
 
-//AppRegistry.registerComponent('main', () => App);
-// copied from https://shopify.github.io/react-native-skia/docs/shapes/path#using-path-object
-const path = Skia.Path.Make();
-path.moveTo(128, 0);
-path.lineTo(168, 80);
-path.lineTo(256, 93);
-path.lineTo(192, 155);
-path.lineTo(207, 244);
-path.lineTo(128, 202);
-path.lineTo(49, 244);
-path.lineTo(64, 155);
-path.lineTo(0, 93);
-path.lineTo(88, 80);
-path.lineTo(128, 0);
-path.close();
-
-const App = () => {
+export default function App() {
   return (
-    <Canvas style={style.container}>
-      <Path path={path} color={"black"} style={"stroke"} strokeWidth={2} />
-    </Canvas>
+    <SafeAreaView style={styles.container}>
+      <SketchCanvasWithInteractionAndCustomization />
+    </SafeAreaView>
   );
-};
+}
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
-export default App;
