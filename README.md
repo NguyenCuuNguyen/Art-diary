fab3de239495f9cbbba6e0dc16f2c914954190d2
NguyenCuuNguyen <nguyen2nk@gmail.com>
2024-01-02 16:38:19 -0500

    current progress

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Art-diary
 Application to draw or upload art, learn more about symbolism and self-expression
+npm start 
--- src/screens/navigation/StackNavigator.js
+++ src/screens/navigation/StackNavigator.js
@@ -6,6 +6,7 @@ import DrawScreen from "../DrawScreen";
 
 const Stack = createStackNavigator();
 
+//TOFIX: once enter draw, need to exit bottom stack. Once back to home, need to re-eneter stack.
 export const MainStackNavigator = () => {
   return (
     <Stack.Navigator>
